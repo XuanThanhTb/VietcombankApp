fdc07e98c9897f99d0834bdc115366b50ace3731
XuanThanhTb <xuanthanh180599@gmail.com>
2020-05-07 20:38:45 +0700

    xong màn login vietcombank

---
+++ app/src/main/java/com/example/vietcombankapp/LoginFragment.kt
@@ -0,0 +1,35 @@
+package com.example.vietcombankapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class LoginFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onLoginVietconbank.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.homeVietcombankFragment)
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = LoginFragment()
+    }
+}
